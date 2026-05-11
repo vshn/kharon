6aa4915aafaf31c95ff884d22091875db62b7d01
Sebastian Widmer <sebastian.widmer@vshn.net>
2026-05-11 21:48:24 +0200

    Shell command

--- go.mod
+++ go.mod
@@ -15,27 +15,48 @@ require (
 	golang.org/x/crypto v0.50.0
 	golang.org/x/oauth2 v0.36.0
 	golang.org/x/sync v0.20.0
+	k8s.io/client-go v0.36.0
 	tailscale.com v1.98.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
 	github.com/go-jose/go-jose/v4 v4.1.4 // indirect
 	github.com/go-json-experiment/json v0.0.0-20250813024750-ebf49471dced // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kisielk/errcheck v1.10.0 // indirect
 	github.com/miekg/dns v1.1.58 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.yaml.in/yaml/v2 v2.4.3 // indirect
 	go4.org/mem v0.0.0-20240501181205-ae6ca9944745 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	golang.org/x/mod v0.35.0 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/term v0.42.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
 	golang.org/x/tools v0.44.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apimachinery v0.36.0 // indirect
+	k8s.io/klog/v2 v2.140.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20260317180543-43fb72c5454a // indirect
+	k8s.io/utils v0.0.0-20260210185600-b8788abfbbc2 // indirect
+	sigs.k8s.io/json v0.0.0-20250730193827-2d320260d730 // indirect
+	sigs.k8s.io/randfill v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v6 v6.3.2 // indirect
+	sigs.k8s.io/yaml v1.6.0 // indirect
 )
 
 replace github.com/kevinburke/ssh_config => ./hack/ssh_config
---
+++ internal/cmd/shell.go
@@ -0,0 +1,98 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+	"github.com/vshn/kharon/internal/pkg/lieutenant"
+	"k8s.io/client-go/tools/clientcmd"
+	kubeconfig "k8s.io/client-go/tools/clientcmd/api"
+)
+
+func init() {
+	RootCmd.AddCommand(shellCmd)
+
+	flag := shellCmd.Flags()
+	flag.StringVar(&clustersInventoryFile, "inventory-file", inventoryFilePath(), "Path to the inventory file that should be written by this command.")
+}
+
+var shellCmd = &cobra.Command{
+	Use:   "shell",
+	Short: "Run a shell with kubeconfig setup to use the proxy.",
+	Long:  "Run a shell with kubeconfig setup to use the proxy. Works on the inventory downloaded by the `update` command, so it does not require access to the Lieutenant API.",
+	Run:   runShell,
+}
+
+func runShell(cmd *cobra.Command, _ []string) {
+	if clustersInventoryFile == "" {
+		slog.Error("Inventory file path is required", "error", "inventory-file flag is empty and failed to determine default path.")
+		os.Exit(1)
+	}
+
+	var clusters []lieutenant.Cluster
+	f, err := os.Open(clustersInventoryFile)
+	if err != nil {
+		slog.Error("Failed to open inventory file", "file", clustersInventoryFile, "error", err)
+		os.Exit(1)
+	}
+	defer func() { _ = f.Close() }()
+	if err := json.NewDecoder(f).Decode(&clusters); err != nil {
+		slog.Error("Failed to decode inventory file", "file", clustersInventoryFile, "error", err)
+		os.Exit(1)
+	}
+
+	kc := kubeconfig.NewConfig()
+	currentContextSet := false
+	for _, c := range clusters {
+		api, _, _ := c.DynamicStringFact("openshiftApiURL")
+		if api == "" {
+			continue
+		}
+		clusterName := c.ID
+		contextName := c.ID
+		kc.Clusters[clusterName] = &kubeconfig.Cluster{
+			Server:   api,
+			ProxyURL: "socks5h://localhost:12000",
+		}
+		kc.Contexts[contextName] = &kubeconfig.Context{
+			Cluster: clusterName,
+		}
+		if !currentContextSet {
+			kc.CurrentContext = contextName
+			currentContextSet = true
+		}
+	}
+	tmpFile, err := os.CreateTemp("", "kharon-shell-*.kubeconfig")
+	if err != nil {
+		slog.Error("Failed to create temporary kubeconfig file", "error", err)
+		os.Exit(1)
+	}
+	_ = f.Close() // We only need the name, so we can close it immediately.
+	tmpFileName := tmpFile.Name()
+	defer func() { _ = os.Remove(tmpFileName) }()
+	if err := clientcmd.WriteToFile(*kc, tmpFileName); err != nil {
+		slog.Error("Failed to write kubeconfig", "error", err)
+		os.Exit(1)
+	}
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		slog.Error("SHELL environment variable is not set")
+		os.Exit(1)
+	}
+	ex := exec.Command(shell)
+	ex.Env = append(os.Environ(), "KUBECONFIG="+tmpFileName)
+	ex.Stdin = os.Stdin
+	ex.Stdout = os.Stdout
+	ex.Stderr = os.Stderr
+	if err := ex.Run(); err != nil {
+		if exitErr, ok := errors.AsType[*exec.ExitError](err); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+		slog.Error("Failed to run shell", "error", err)
+		os.Exit(1)
+	}
+}
