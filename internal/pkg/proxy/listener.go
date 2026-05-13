0c9f82fb248da50229019d8a0fa6af9a8d8c1d90
Sebastian Widmer <widmer.sebastian@gmail.com>
2026-05-13 17:33:04 +0200

    Proxy connection count: Fix incorrect reporting on connection double close (#17)

--- internal/pkg/proxy/listener.go
+++ internal/pkg/proxy/listener.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"net"
+	"sync"
 	"sync/atomic"
 )
 
@@ -10,20 +11,23 @@ type ConnCountingListener struct {
 	connCount atomic.Int64
 }
 
-type CountingConn struct {
+type countingConn struct {
 	net.Conn
-	connCount *atomic.Int64
+
+	subtractOnce sync.Once
+	connCount    *atomic.Int64
 }
 
-func (c CountingConn) Close() error {
-	c.connCount.Add(-1)
+func (c *countingConn) Close() error {
+	defer c.subtractOnce.Do(func() { c.connCount.Add(-1) })
 	return c.Conn.Close()
 }
 
 func (l *ConnCountingListener) Accept() (net.Conn, error) {
 	conn, err := l.Listener.Accept()
-	if err == nil {
-		l.connCount.Add(1)
+	if err != nil {
+		return nil, err
 	}
-	return CountingConn{Conn: conn, connCount: &l.connCount}, err
+	l.connCount.Add(1)
+	return &countingConn{Conn: conn, connCount: &l.connCount}, nil
 }
