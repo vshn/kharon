441acb1cf83102ff9b600cb8a73bd5e01752376e
Sebastian Widmer <sebastian.widmer@vshn.net>
2026-06-02 21:41:41 +0200

    Add homebrew cask

---
+++ hack/homebrew/kharon.rb
@@ -0,0 +1,59 @@
+cask "kharon" do
+  os macos: "darwin", linux: "linux"
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "{{ .version }}"
+  sha256 arm:          "{{ .sha256_arm }}",
+         x86_64_linux: "{{ .sha256_x86_64_linux }}",
+         arm64_linux:  "{{ .sha256_arm64_linux }}"
+
+  url "https://github.com/vshn/kharon/releases/download/#{version}/kharon-#{os}-#{arch}"
+  name "Kharon"
+  desc "Kharon ferries your connections safely across SSH jumphosts into private networks."
+  homepage "https://github.com/vshn/kharon"
+
+  kharon_binary = "#{staged_path}/kharon-#{os}-#{arch}"
+
+  install_script = <<~SHELL
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+      xattr -dr com.apple.quarantine '#{kharon_binary}'
+      codesign -s - --deep --force '#{kharon_binary}'
+    fi
+    chmod +x '#{kharon_binary}'
+    '#{kharon_binary}' completion bash > '#{staged_path}/kharon-completion.bash'
+    '#{kharon_binary}' completion zsh > '#{staged_path}/_kharon'
+    '#{kharon_binary}' completion fish > '#{staged_path}/kharon-completion.fish'
+    '#{kharon_binary}' install --yes > /dev/null
+  SHELL
+
+  installer script: {
+    executable: "sh",
+    args:       ["-e", "-c", install_script],
+  }
+
+  binary kharon_binary, target: "kharon"
+
+  bash_completion "#{staged_path}/kharon-completion.bash", target: "kharon"
+  zsh_completion "#{staged_path}/_kharon", target: "_kharon"
+  fish_completion "#{staged_path}/kharon-completion.fish", target: "kharon.fish"
+
+  uninstall launchctl: "io.vshn.Kharon",
+            trash: [
+              "~/Library/LaunchAgents/io.vshn.kharon.plist",
+              "~/Library/Logs/io.vshn.Kharon.err.log",
+              "~/Library/Logs/io.vshn.Kharon.out.log",
+            ]
+
+  zap trash: [
+    "~/Library/Caches/io.vshn.kharon",
+    "~/Library/Application Support/io.vshn.kharon",
+  ]
+
+  caveats do
+    <<~EOS
+      Run `kharon update` to receive jumphost and cluster information.
+
+      Setup your browser to use Kharon! https://github.com/vshn/kharon/tree/main/docs/setup
+    EOS
+  end
+end
